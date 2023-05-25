d6fa4a5961efcdb2e1afa0f58951fe839cd2b3a1
RolandMarton <roland111187@gmail.com>
2023-05-25 19:50:20 +0200

    Make every dummy data working

--- lib/data/dummy_items.dart
+++ lib/data/dummy_items.dart
@@ -1,7 +1,8 @@
 import 'package:shopping_list_app/models/grocery_item.dart';
 import 'package:shopping_list_app/data/categories.dart';
+import 'package:shopping_list_app/models/category.dart';
 
-const groceryItems = [
+final groceryItems = [
   GroceryItem(
       id: 'a',
       name: 'Milk',
